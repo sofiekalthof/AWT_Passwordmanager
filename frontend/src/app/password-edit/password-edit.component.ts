ee5331ae6176d83a9f1f8791dd6a486c6de8f36b
sofiekalthof <sofie.kalthof@stud.uni-due.de>
2023-06-05 20:58:12 +0200

    Added all important things to angular app

--- frontend/src/app/password-edit/password-edit.component.ts
+++ frontend/src/app/password-edit/password-edit.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PasswordService } from '../password.service';
 import { Password } from '../models/password';
 import { ActivatedRoute, Router } from '@angular/router';
 
@@ -15,12 +16,60 @@ export class PasswordEditComponent {
   constructor(
     private route: ActivatedRoute, // Provides access to the current route
     private router: Router, // Used for navigation
+    private passwordService: PasswordService // Service for managing passwords
   ) { }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id')!; // Retrieves the 'id' parameter from the route
+    
+    if (id !== null) {
+      // Fetch the password details from the password service based on the ID
+      this.passwordService.getPasswordById(id).subscribe((password: Password) => {
+        this.password = password;
+        this.decryptedPassword = atob(password.encryptedPassword);
+        // debug line
+        console.log(this.password);
+      });
+    } else {
+      // Set default values for a new password
+      this.password = {
+        category: '',
+        app: '',
+        userName: '',
+        encryptedPassword: ''
+      };
+    }
   }
 
+  save(): void {
+    // Check if all required fields are filled
+    if (!this.password.category || !this.password.app || !this.password.userName || !this.decryptedPassword) {
+      alert('Please enter all the fields!');
+      return;
+    }
 
+    this.isSaving = true; // Set saving flag to true
+    
+    if (this.password._id === undefined) {
+      // Add a new password
+      this.password.encryptedPassword = btoa(this.decryptedPassword); // Encrypt the password
+      this.passwordService.addPassword(this.password)
+        .subscribe(() => {
+          this.isSaving = false; // Set saving flag to false
+          this.goBack(); // Navigate back
+        });
+    } else {
+      // Update an existing password
+      if (this.decryptedPassword) {
+        this.password.encryptedPassword = btoa(this.decryptedPassword); // Encrypt the password if it exists
+      }
+      this.passwordService.updatePassword(this.password._id!, this.password)
+        .subscribe(() => {
+          this.isSaving = false; // Set saving flag to false
+          this.goBack(); // Navigate back
+        });
+    }
+  }
 
   goBack(): void {
     this.router.navigate(['/']); // Navigate back to the password manager page
--- frontend/src/app/password-manager/password-manager.component.ts
+++ frontend/src/app/password-manager/password-manager.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Password } from '../models/password';
-
+import { PasswordService } from '../password.service';
 
 @Component({
   selector: 'app-password-manager',
@@ -9,8 +9,27 @@ import { Password } from '../models/password';
 })
 export class PasswordManagerComponent implements OnInit{
   passwords!: Password[];
+  constructor(private passwordService: PasswordService) {}
 
   ngOnInit(): void {
+    this.getPasswords(); // Retrieves the passwords when the component is initialized
   }
+
+  getPasswords(): void {
+    // Retrieves all passwords from the password service
+    this.passwordService.getAllPasswords()
+      .subscribe(passwords => this.passwords = passwords);
+  }
+
+  deletePassword(password: Password) {
+    if (confirm('Are you sure you want to delete this password?')) {
+      // Removes the password from the list
+      this.passwords = this.passwords.filter(p => p !== password);
+      // Calls the password service to delete the password
+      this.passwordService.deletePassword(password._id!).subscribe();
+    }
+  }
+
+
 
 }
