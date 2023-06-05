d1a24933c15fb01c2aa5679a7dea007bb9394390
sofiekalthof <sofie.kalthof@stud.uni-due.de>
2023-06-05 21:28:36 +0200

    Added missing modules

--- backend/src/dbConnection.js
+++ backend/src/dbConnection.js
@@ -2,16 +2,14 @@ const mongoose = require("mongoose");
 const express = require("express");
 require('dotenv').config();
 
-const app = express();
-
 // Initialize parameters
 const dbName = process.env.DB_NAME;
 
 // database connection string
 const dbUrl = process.env.MONGODB_URL;
 
+// define port
 const port = 3600;
-
 
 // create database connection
 mongoose.connect(dbUrl, {
@@ -19,10 +17,6 @@ mongoose.connect(dbUrl, {
     })
     .then(() => {
       console.log("Connected to DB");
-      // start listening to the port
-      app.listen(port, () => {
-        console.log("Listening on " + port + ".");
-      }); 
     })
     .catch((err) => {
       console.log("Error connecting to DB", err);
