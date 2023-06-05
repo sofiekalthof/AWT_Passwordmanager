f8698d28ce58ccbfc31c9b5207df2421272ad0b4
sofiekalthof <sofie.kalthof@stud.uni-due.de>
2023-06-05 17:42:50 +0200

    Added port and listen to server

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,11 +3,15 @@ const express = require("express");
 // Create Express app
 const app = express();
 
+// Define port
+const port = 3600
 
 // Enable parsing of JSON bodies
 app.use(express.json());
 
-
-
+// start listening to the port
+app.listen(port, () => {
+    console.log("Listening on " + port + ".");
+  }); 
 
 
